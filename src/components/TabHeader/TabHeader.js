b9544d4c43dfb9f1738dfb9a489b6c438296c00f
Glib <g.osypenko@gmail.com>
2025-04-07 13:51:52 +0300

    додана зміна зображення кнопки старт стоп

--- src/components/TabHeader/TabHeader.js
+++ src/components/TabHeader/TabHeader.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TabHeader.css'; 
 import metronomePIC from '../Images/metronomePIC.png';
+import stopPIC from '../Images/stopPIC.png';
+import playPIC from '../Images/playPIC.png';
 
 const TabHeader = () => {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlaying((prev) => !prev);
+  };
+
   return (
     <header className="tab-header">
       <div className="tab-header-buttons">
-        <button className="play">▶</button>
-        <button className="stop">⏹</button>
+        <button className="play"onClick={togglePlay}>
+         <img 
+            src={isPlaying ? stopPIC : playPIC} 
+            alt={isPlaying ? "Stop" : "Play"} 
+            className="icon"
+          />
+        </button>
+
         <button className="metronomeButton">
         <img src={metronomePIC} alt Metronome/>
         </button>
