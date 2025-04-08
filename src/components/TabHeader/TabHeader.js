6efbf7e46d1c7ed8b1fd60310b19470dcd139b90
zolotonos <zolotonos.ua@gmail.com>
2025-04-08 17:15:48 +0300

    Кнопки

--- src/components/TabHeader/TabHeader.js
+++ src/components/TabHeader/TabHeader.js
@@ -7,16 +7,57 @@ import playPIC from '../Images/playPIC.png';
 const TabHeader = () => {
 
   const [isPlaying, setIsPlaying] = useState(false);
+  const [timeSignature, setTimeSignature] = useState('4/4');
 
   const togglePlay = () => {
     setIsPlaying((prev) => !prev);
   };
 
+  const isValidTimeSignature = (value) => {
+    const regex = /^([1-9][0-9]*)\/(2|4|8|16)$/;
+    return regex.test(value);
+  };
+
+  const handleTimeSignatureChange = (e) => {
+    setTimeSignature(e.target.value);
+  };
+
   return (
     <header className="tab-header">
       <div className="tab-header-buttons">
-        <button className="play"onClick={togglePlay}>
-         <img 
+
+        <div className={`speed-control time-signature-control ${isValidTimeSignature(timeSignature) ? '' : 'invalid'}`}>
+          <input
+            type="text"
+            value={timeSignature}
+            onChange={handleTimeSignatureChange}
+            placeholder="4/4"
+          />
+        </div>
+
+        <div className="speed-control bpm-control">
+          <input
+            type="number"
+            id="bpmInput"
+            min="20"
+            max="300"
+            defaultValue={120}
+          />
+          <span className="bpm-label">BPM</span>
+        </div>
+
+        <div className="speed-control">
+          <input
+            type="number"
+            id="speedInput"
+            min="0"
+            defaultValue={100}
+          />
+          <span className="percent-label">%</span>
+        </div>
+
+        <button className="play" onClick={togglePlay}>
+          <img 
             src={isPlaying ? stopPIC : playPIC} 
             alt={isPlaying ? "Stop" : "Play"} 
             className="icon"
@@ -24,7 +65,7 @@ const TabHeader = () => {
         </button>
 
         <button className="metronomeButton">
-        <img src={metronomePIC} alt="Metronome"/>
+          <img src={metronomePIC} alt="Metronome"/>
         </button>
       </div>
     </header>
