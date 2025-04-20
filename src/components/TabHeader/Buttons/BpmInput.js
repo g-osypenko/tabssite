702dd862db9de50a4112c12b302eb982b150cccb
Glib <g.osypenko@gmail.com>
2025-04-21 01:54:21 +0300

    Додавання обмеження значення кнопки BPM

--- src/components/TabHeader/Buttons/BpmInput.js
+++ src/components/TabHeader/Buttons/BpmInput.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-const BpmInput = () => (
-  <div className="speed-control bpm-control">
-    <input
-      type="number"
-      id="bpmInput"
-      min="20"
-      max="300"
-      defaultValue={120}
-    />
-    <span className="bpm-label">BPM</span>
-  </div>
-);
+const BpmInput = () => {
+  useEffect(() => {
+    const input = document.getElementById("bpmInput");
+    input.addEventListener("input", () => {
+      const value = parseInt(input.value, 10);
+      if (value > 400) input.value = 400;
+      if (value < 2) input.value = 1;
+    });
+  }, []);
+
+  return (
+    <div className="speed-control bpm-control">
+      <input
+        type="number"
+        id="bpmInput"
+        min={1}
+        max={400}
+        defaultValue={120}
+      />
+      <span className="bpm-label">BPM</span>
+    </div>
+  );
+};
 
 export default BpmInput;
